eea9f3eef8bb58c85ed98cf43f8219326536a5a2
Louie <louie@Zipcoders-MacBook-Pro-3.local>
2023-10-30 18:22:27 -0400

    Finished TableUtilities

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -2,14 +2,31 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+        return getMultiplicationTable(5);
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+        return getMultiplicationTable(10);
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i <= tableSize; i++) {
+            for (int j = 1; j <= tableSize; j++) {
+                int product = j * i;
+                boolean ones = false, tens = false, hundreds = false;
+                if (product < 10) {
+                    sb.append("  ");
+                }
+                else if (product < 100) {
+                    sb.append(" ");
+                }
+                sb.append(product);
+                sb.append(" |");
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
     }
 }
